7fc7eb04d15c0accb8b7e5d6d669bb8d4f8e6f54
VIKTA DZY <viktamuhumuza@gmail.com>
2022-06-29 17:21:50 +0300

    Initial commit

---
+++ app/src/main/java/ItemsViewModel.kt
@@ -0,0 +1,5 @@
+package com.viktadzy.recycleviewapp
+
+import android.widget.ImageView
+
+data class ItemsViewModel(val image: Int, val text:String){}
---
+++ app/src/main/java/com/viktadzy/recycleviewapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.viktadzy.recycleviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recylcerv = findViewById<RecyclerView>(R.id.recyc_view)
+        recylcerv.layoutManager = LinearLayoutManager(this)
+
+        val data = ArrayList<ItemsViewModel>()
+        for (i in 1..20) {
+            data.add(ItemsViewModel(R.drawable.ic_baseline_person_24, "item" + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = customAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recylcerv.adapter = adapter
+    }
+}
